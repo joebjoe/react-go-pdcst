87b3479dfa0bb9829f7ca4e7648f2eb6c6a782f5
Joe Ingoglia <joei@acst.com>
2021-02-22 09:11:30 -0500

    redirect testing

---
+++ src/components/Nav/index.js
@@ -0,0 +1,29 @@
+import './index.css';
+import { NavLink } from 'react-router-dom';
+
+export function navRoute(name, path) {
+  return {
+    name: name,
+    path: `/${path === undefined ? name : path }`.toLowerCase(),
+  }
+}
+
+export default function(props) {
+  return (
+    <nav className={props.className}>
+      <ul>
+        {props.routes.map(route => (
+          <li key={route.name}>
+            <NavLink
+              exact
+              to={route.path}
+              onClick={props.onClick}
+            >
+              {route.name}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
---
+++ src/components/View/index.js
@@ -0,0 +1,10 @@
+import './index.css';
+
+export default function(props) {
+  return (
+    <section className={"view view--" + props.className}>
+      <h2>{props.title}.</h2>
+      {props.children}
+    </section>
+  )
+}
