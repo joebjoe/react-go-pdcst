2c46586c265be209f5886d79f6b8f65d9401a46a
Joe Ingoglia <joei@acst.com>
2021-02-28 06:21:08 -0500

    env troubleshooting

--- src/common.js
+++ src/common.js
@@ -1,6 +1,6 @@
 const get_url = (path, data) => {
     let query = Object.entries(data).filter(e => e[1]).map(e => `${e[0]}=${e[1]}`).join('&');
-    return encodeURI(`${process.env.REACT_APP_API}/${path}${query ? '?' + query : ''}`)
+    return encodeURI(`/api/v1/${path}${query ? '?' + query : ''}`)
 };
 
 export { get_url };
