1e4e3bb2e68444ad09fbf6ee99f64bc4d6b2c70b
Joe Ingoglia <joei@acst.com>
2021-03-03 17:06:55 -0500

    remove console stmt

--- src/components/NavBtn/index.js
+++ src/components/NavBtn/index.js
@@ -1,7 +1,6 @@
 import { GrMenu, GrClose } from "react-icons/gr";
 export default function(props) {
     const NavBtn = props.show ? GrClose : GrMenu
-    console.log(props);
     return (
         <NavBtn
             color="#5f5f5f"
